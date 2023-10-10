cde62d897244a5300d98ece206aadd4b63f4863b
amieldelatorre <amieljames.delatorre@gmail.com>
2023-10-11 01:04:11 +1300

    Add error modal for showing validation errors

--- frontend/html/scripts/signup.js
+++ frontend/html/scripts/signup.js
@@ -10,11 +10,59 @@ function validatePasswordMatch() {
   }
 }
 
+function setValidationErrorModal(errorJSONPromise) {
+  const errorModal = document.getElementById("signup-form-error-modal");
+  let errorModalHeading = document.createElement("h2");
+  errorModalHeading.id = "error-modal-heading";
+  errorModalHeading.innerText = "Input Errors!";
 
-window.addEventListener("load", () => {
+  errorModal.appendChild(errorModalHeading);
+
+
+  errorJSONPromise.then(errorJSON => {
+  
+    for (const [errorFieldName, fieldErrorList] of Object.entries(errorJSON)) {
+      let newErrorSection = document.createElement("section");
+
+      let newErrorSectionLabel = document.createElement("p");
+      newErrorSectionLabel.innerText = errorFieldName;
+      newErrorSection.appendChild(newErrorSectionLabel);
+  
+      let newErrorSectionList = document.createElement("ul");
+      newErrorSection.appendChild(newErrorSectionList);
+      fieldErrorList.forEach((elem) => {
+        let newErrorItem = document.createElement("li")
+        newErrorItem.innerText = elem;
+  
+        newErrorSectionList.appendChild(newErrorItem);
+      });
+  
+      errorModal.appendChild(newErrorSection); 
+    }
+  
+  });
+  errorModal.style.display = "block";
+}
+
+function setUnableToProcessRequestModal() {
+  const errorModal = document.getElementById("signup-form-error-modal");
+
+  let errorModalHeading = document.createElement("h2");
+  errorModalHeading.id = "error-modal-heading";
+  errorModalHeading.innerText = "Unable to process request 😞";
+  errorModal.appendChild(errorModalHeading);
+
+  let errorModalText = document.createElement("p");
+  errorModalText.innerText = "Please try again later and if problem persists, please contact the administrator.";
+  errorModal.appendChild(errorModalText);
+
+  errorModal.style.display = "block";
+}
+
+function handleSignupFormSubmit() {
   let loginForm = document.getElementById("signup-form");
-  loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  loginForm.addEventListener("submit", async (submitEvent) => {
+    submitEvent.preventDefault();
 
     let firstName = document.getElementById("firstName").value;
     let lastName = document.getElementById("lastName").value;
@@ -34,16 +82,68 @@ window.addEventListener("load", () => {
       "height": height
     }
 
-    fetch(URL=`${API_URL}/User`, {
+    await fetch(URL=`${API_URL}/User`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(data)
     })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+    .then(response => {
+      if (response.ok) {
+        window.location.replace(`/login?signup=success`)
+      }
+      else {
+        if (response.status === 422) {
+          let errorJSONPromise = response.json().then(err => err["errors"]);
+          setValidationErrorModal(errorJSONPromise);
+        } else {
+          setUnableToProcessRequestModal();
+        }
+      }
+    })
+    .catch(error => {
+      alert(error)
+    });
   });
+}
+
+window.addEventListener("load", () => {
+  handleSignupFormSubmit();
 });
 
+// window.addEventListener("load", async () => {
+//   let data = {
+//     "firstName": "firstName",
+//     "lastName": "lastName",
+//     "email": "james.smith@example.com",
+//     "password": "password",
+//     "dateOfBirth": "2023-10-10",
+//     "gender": "MALE",
+//     "height": 1
+//   }
+
+//   await fetch(URL=`${API_URL}/User`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(data)
+//   })
+//   .then(res => {
+//     if (res.ok) {
+//       window.location.replace(`/login?signup=success`)
+//     }
+//     else {
+//       if (res.status === 422) {
+//         let errorJSONPromise = res.json().then(err => err["errors"]);
+//         setValidationErrorModal(errorJSONPromise);
+//       } else {
+//         setUnableToProcessRequestModal();
+//       }
+//     }
+//   })
+//   .catch(err => {
+//     alert(err)
+//   });
+// });
