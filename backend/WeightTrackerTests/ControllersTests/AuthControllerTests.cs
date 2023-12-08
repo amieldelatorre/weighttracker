a1726489860b10350a1c6408cc45c85c8b9eefbf
amieldelatorre <amieljames.delatorre@gmail.com>
2023-12-08 21:07:45 +1300

    Update AuthControllerTest

--- backend/WeightTrackerTests/ControllersTests/AuthControllerTests.cs
+++ backend/WeightTrackerTests/ControllersTests/AuthControllerTests.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 using WeightTracker.Authentication;
 using WeightTracker.Controllers;
 using WeightTracker.Data;
+using WeightTracker.Enums;
 using WeightTracker.Models.User;
 
 namespace WeightTrackerTests.ControllersTests
@@ -19,34 +20,49 @@ namespace WeightTrackerTests.ControllersTests
     internal class AuthControllerTests
     {
         private readonly AuthController _authController;
-        private readonly Mock<IUserRepo> _mockUserRepo;
+        private readonly IUserRepo _userRepo;
         private readonly ILogger<AuthController> _logger;
+        private readonly WeightTrackerDbContext _context;
+        private readonly User _existingUser;
 
         public AuthControllerTests()
         {
-            _mockUserRepo = new Mock<IUserRepo>();
+            SQLiteContext sQLiteContext = new();
+            _context = sQLiteContext.CreateSQLiteContext();
+            _userRepo = new PgUserRepo(_context);
             _logger = Mock.Of<ILogger<AuthController>>();
-            _authController = new AuthController(_logger, _mockUserRepo.Object);
+            _authController = new AuthController(_logger, _userRepo);
+            _existingUser = new User()
+            {
+                FirstName = "James",
+                LastName = "Smith",
+                Email = "james.smith@example.com",
+                Password = BCrypt.Net.BCrypt.HashPassword("password"),
+                DateOfBirth = DateOnly.FromDateTime(DateTime.Now.AddDays(-9132)),
+                Gender = GenderEnum.MALE,
+                Height = 165,
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now
+            };
+        }
+
+        [OneTimeSetUp]
+        public void TestSetUp()
+        {
+            _context.AddRange(_existingUser);
+            _context.SaveChanges();
         }
 
         [OneTimeTearDown]
         public void TestTearDown()
         {
             _authController.Dispose();
+            _context.Dispose();
         }
 
         [Test, TestCaseSource(nameof(AuthControllerLoginTestProvider))]
-        public void LoginTest(bool isValidLogin, int statusCode)
+        public void LoginTest(UserLogin userLogin, int statusCode)
         {
-            UserLogin userLogin = new()
-            {
-                Email = "email@email.com",
-                Password = "password"
-            };
-
-            _mockUserRepo.Setup(repo => repo.IsValidLogin(It.IsAny<string>(), It.IsAny<string>()))
-                .Returns(Task.FromResult(isValidLogin));
-
             var result = _authController.Login(userLogin).GetAwaiter().GetResult();
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
 #pragma warning disable CS8605 // Unboxing a possibly null value.
@@ -60,12 +76,38 @@ namespace WeightTrackerTests.ControllersTests
         [
             new object[]
             {
-                false,  // isValidLogin
+                new UserLogin()
+                {
+                    Email = "email@email.com",
+                    Password = "password"
+                },
                 401     // status code expected       
             },
             new object[]
             {
-                true,  // isValidLogin
+                new UserLogin()
+                {
+                    Email = "james.smith@example.com",
+                    Password = "password1"
+                },
+                401     // status code expected       
+            },
+            new object[]
+            {
+                new UserLogin()
+                {
+                    Email = "james.smith1@example.com",
+                    Password = "password"
+                },
+                401     // status code expected       
+            },
+            new object[]
+            {
+                new UserLogin()
+                {
+                    Email = "james.smith@example.com",
+                    Password = "password"
+                },
                 200     // status code expected       
             }
         ];
